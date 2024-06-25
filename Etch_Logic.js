e7a72c61603349a1f676a814d52db6069098b9c6
Riley <169967357+CatdadR@users.noreply.github.com>
2024-06-25 18:19:44 +1000

    Add logic to randomColour function via random rgb values

--- Etch_Logic.js
+++ Etch_Logic.js
@@ -1,19 +1,25 @@
 let gridBody = document.getElementsByClassName("gridBody")[0];
-let gridSize = 16  
-
-let colourSet
+let gridSize = 16;  
+let randomColour = false;
 
 window.onload = function pageLoad(){
     generateCanvas();
-}
+};
+
+function clearCanvas(){
+    for (let i = 0; i < gridSize; i++){
+        gridBody.removeChild(gridBody.firstElementChild);
+        };
+    generateCanvas();
+};
 
 function newCanvas(){ 
     for (let i = 0; i < gridSize; i++){
-    gridBody.removeChild(gridBody.firstElementChild);
-    }
+        gridBody.removeChild(gridBody.firstElementChild);
+    };
     gridSize = prompt("Enter new canvas size")   
     generateCanvas();
-}
+};
 
 function generateCanvas(){   
     if (prompt != null){
@@ -27,14 +33,24 @@ function generateCanvas(){
                 gridCel.className = 'gridCel';
                 gridCel.addEventListener("mouseover", changeColour);
                 gridRow.appendChild(gridCel);
-            }
+            };
         };
     }; 
-}
+};
 
 function changeColour(event){
-    event.currentTarget.style.backgroundColor = '#AD8B73';  
-}
+
+    if (randomColour == true){   
+        let r =  Math.random() * (255 - 0) + 0;
+        let g =  Math.random() * (255 - 0) + 0;
+        let b =  Math.random() * (255 - 0) + 0;
+        const randomRgb = 'rgba(' + r + ',' + g + ',' + b + ')'   
+        event.currentTarget.style.backgroundColor = randomRgb;   
+    } else {
+        event.currentTarget.style.backgroundColor = 'rgba(173, 139, 115)';  
+    }
+
+};
 
 
-
+ 
